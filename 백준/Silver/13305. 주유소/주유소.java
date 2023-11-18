1a9f19c635beb17f5d1ced67f7bd75dc9406a2dd
Young Min Kim <127214889+zrminkim@users.noreply.github.com>
2023-11-18 22:00:59 +0900

    [Silver III] Title: 주유소, Time: 640 ms, Memory: 69240 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.java
+++ 백준/Silver/13305. 주유소/주유소.java
@@ -1,42 +1,44 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.IOException;
+import java.io.*;
+import java.math.BigInteger;
 import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         // 도시 갯수
         int N = Integer.parseInt(br.readLine());
         // 거리
-        int[] dist = new int[N-1];
+        BigInteger[] dist = new BigInteger[N-1];
         // 가격
-        int[] price = new int[N];
+        BigInteger[] price = new BigInteger[N];
 
         StringTokenizer st = new StringTokenizer(br.readLine());
         // 거리 저장
         for(int i=0; i < N-1; i++){
-            dist[i] = Integer.parseInt(st.nextToken());
+            dist[i] = new BigInteger(st.nextToken());
         }
         // 가격 저장
         st = new StringTokenizer(br.readLine());
         for (int k=0; k < N; k++){
-            price[k] = Integer.parseInt(st.nextToken());
+            price[k] = new BigInteger(st.nextToken());
         }
-        // 합계
-        int sum =0;
 
-        //최소 비용
-        int min = price[0];
+        //처음 주유소 비용은 무조건 발생
+        BigInteger result = price[0].multiply(dist[0]);
+        BigInteger minPrice = price[0];
 
-        for(int i=0; i < N-1; i++){
-            if(price[i] < min){
-                min = price[i];
+        for(int i=1; i < price.length -1; i++){
+            if(price[i].compareTo(minPrice) < 0){
+                result = result.add(price[i].multiply(dist[i]));
+                minPrice = price[i];
+            }else {
+                result = result.add(minPrice.multiply(dist[i]));
             }
-            sum += min * dist[i];
         }
 
-        System.out.println(sum);
+        bw.write(String.valueOf(result));
+        bw.close();
         br.close();
     }
 }
